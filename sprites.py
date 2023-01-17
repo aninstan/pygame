e8f430444518b061e365da785af8032b12098216
aninstan <e-anista@asvg.no>
2023-01-17 19:05:22 +0100

    functioning sprites

--- sprites.py
+++ sprites.py
@@ -10,21 +10,25 @@ SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Spritesheets')
 
-sprite_sheet_image = pygame.image.load('assets/character_walking.png').convert_alpha()
+sprite_sheet_image = pygame.image.load('assets/character_animations.png').convert_alpha()
 sprite_sheet = spritesheets.SpriteSheet(sprite_sheet_image)
 
 BLACK = (0, 0, 0)
 BG = (50, 50, 50)
 
-#walking animation: down left, down right, up right, up left
+#idle animation: 0 down left, 1 down right, 2 up right, 3 up left
+#walking animation: 4 down right, 5 down left, 6 up right, 7 up left
 
 animation_list = []
-animation_steps = [4, 4, 4, 4]
+animation_steps = [4, 4, 4, 4, 4, 4, 4, 4]
 action = 0
 last_update = pygame.time.get_ticks()
 animation_cooldown = 150
 frame = 0
 step_counter = 0
+
+
+
 
 for animation in animation_steps:
     temp_img_list = []
@@ -43,7 +47,7 @@ while run:
     if current_time - last_update >= animation_cooldown:
         frame += 1
         last_update = current_time
-        if frame >= len(animation_list):
+        if frame >= animation_steps[action]:
             frame = 0
 
     screen.blit(animation_list[action][frame], (100, 100))
